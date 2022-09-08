fc381911d9e8958c24a3952fb8044cf524116d76
+ <+>
2022-09-08 09:01:41 +0300

    Second submission

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//Coded Solution
 
 const totalBatteries = batteryBatches.reduce((accumulator,currentValue)=>{
     return accumulator+currentValue;
